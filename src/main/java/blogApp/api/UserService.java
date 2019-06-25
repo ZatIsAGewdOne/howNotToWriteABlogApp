fbf253cd4b5842a0e8dcbb1ba0836b62971e7207
ZatIsAGewdOne <edva331@gmail.com>
2019-06-25 09:35:17 +0300

    Updating blog app

--- src/main/java/blogApp/api/UserService.java
+++ src/main/java/blogApp/api/UserService.java
@@ -5,6 +5,7 @@ import blogApp.database.entities.User;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,7 +33,6 @@ public class UserService {
 
     @PostMapping(value = "login")
     public void login(HttpServletRequest request, HttpServletResponse response, @RequestParam("name") String name, @RequestParam("password") String password) {
-        response.reset();
         User user = userRepository.findByName(name);
         if(user != null && BCrypt.checkpw(password, user.getPassword())) {
             HttpSession session = request.getSession();
--- src/main/java/blogApp/database/UserRepository.java
+++ src/main/java/blogApp/database/UserRepository.java
@@ -4,10 +4,8 @@ import blogApp.database.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer>, org.springframework.data.repository.Repository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
     // This implements CRUD functionality to User table in database
 
     User findByName(String name);
